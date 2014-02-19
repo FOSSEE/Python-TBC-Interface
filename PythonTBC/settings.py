80043faf74e19064f040a132e3189d3e516c4300
hardythe1 <hardy_the1@yahoo.com>
2014-02-19 18:45:53 +0530

    setting mysql username

--- PythonTBC/settings.py
+++ PythonTBC/settings.py
@@ -19,7 +19,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'pythontbc',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
+        'USER': 'pythontbc',
         'PASSWORD': DATABASE_PASSWORD,
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.

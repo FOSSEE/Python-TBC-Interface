f81da73b10ac16222f903acc5e93245de3a64b5a
mahesh <mahesh_gudi@iitb.ac.in>
2016-04-01 11:51:55 +0530

    changed urls

--- PythonTBC/urls.py
+++ PythonTBC/urls.py
@@ -22,13 +22,9 @@ urlpatterns = patterns('',
     url(r'^', include('tbc.urls', namespace='tbc')),
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
 
-    url(r'^admin-tools/$', 'tbc.views.admin_tools'),
     url(r'^admin-tools/commenting', 'commentingapp.views.commenting', name = 'commenting'),
     url(r'^admin-tools/error_page', 'tbc_error_page.views.error', name = 'error_page'),
     url(r'^admin-tools/broken_page', 'tbc_error_page.views.broken', name = 'broken_page'),
-
-
-
    
 )
 
